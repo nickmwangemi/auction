f5a41423445e256fba09b98c8dac2bfddeecc26c
nickmwangemi <nickmwangemi@gmail.com>
2025-02-18 19:12:35 +0300

    linting

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-
 from django.utils import timezone
 
 
@@ -20,7 +19,6 @@ class Auction(models.Model):
             return "Active"
         else:
             return "Ended"
-
 
 
 class Listing(models.Model):

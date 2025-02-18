1542f7fe0657f1a88d78681baee690d703df2f3c
nickmwangemi <nickmwangemi@gmail.com>
2025-02-18 18:34:01 +0300

    linting

--- auction_api/views.py
+++ auction_api/views.py
@@ -1,14 +1,19 @@
 from rest_framework import viewsets
-from auctions.models import Auction, Listing, Bid
-from .serializers import AuctionSerializer, ListingSerializer, BidSerializer
+
+from auctions.models import Auction, Bid, Listing
+
+from .serializers import AuctionSerializer, BidSerializer, ListingSerializer
+
 
 class AuctionViewSet(viewsets.ModelViewSet):
     queryset = Auction.objects.all()
     serializer_class = AuctionSerializer
 
+
 class ListingViewSet(viewsets.ModelViewSet):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+
 
 class BidViewSet(viewsets.ModelViewSet):
     queryset = Bid.objects.all()
--- auctions/urls.py
+++ auctions/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from .views import ListingListView, ListingDetailView, PlaceBidView, HomeView, SetAuctionView, CreateListingView
+
+from .views import (CreateListingView, HomeView, ListingDetailView,
+                    ListingListView, PlaceBidView, SetAuctionView)
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('listings/', ListingListView.as_view(), name='listing_list'),
-    path('listing/<int:pk>/', ListingDetailView.as_view(), name='listing_detail'),
-    path('listing/<int:listing_id>/bid/', PlaceBidView.as_view(), name='place_bid'),
-    path('set-auction/', SetAuctionView.as_view(), name='set_auction'),
-    path('create-listing/<int:auction_id>/', CreateListingView.as_view(), name='create_listing'),
+    path("", HomeView.as_view(), name="home"),
+    path("listings/", ListingListView.as_view(), name="listing_list"),
+    path("listing/<int:pk>/", ListingDetailView.as_view(), name="listing_detail"),
+    path("listing/<int:listing_id>/bid/", PlaceBidView.as_view(), name="place_bid"),
+    path("set-auction/", SetAuctionView.as_view(), name="set_auction"),
+    path(
+        "create-listing/<int:auction_id>/",
+        CreateListingView.as_view(),
+        name="create_listing",
+    ),
 ]

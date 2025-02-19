62f85549ae0b4245794061880142183009cbfb6f
nickmwangemi <nickmwangemi@gmail.com>
2025-02-19 14:09:42 +0300

    linting

--- auction_api/serializers.py
+++ auction_api/serializers.py
@@ -6,21 +6,39 @@ from auctions.models import Bid, Listing, Auction
 class BidSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bid
-        fields = ['id', 'listing', 'user', 'amount', 'timestamp', 'is_below_base_price']
-        read_only_fields = ['id', 'user', 'timestamp', 'is_below_base_price']
+        fields = ["id", "listing", "user", "amount", "timestamp", "is_below_base_price"]
+        read_only_fields = ["id", "user", "timestamp", "is_below_base_price"]
+
 
 class ListingSerializer(serializers.ModelSerializer):
     bids = BidSerializer(many=True, read_only=True)
 
     class Meta:
         model = Listing
-        fields = ['id', 'auction', 'title', 'description', 'base_price', 'start_time', 'end_time', 'bids']
-        read_only_fields = ['id', 'bids']
+        fields = [
+            "id",
+            "auction",
+            "title",
+            "description",
+            "base_price",
+            "start_time",
+            "end_time",
+            "bids",
+        ]
+        read_only_fields = ["id", "bids"]
+
 
 class AuctionSerializer(serializers.ModelSerializer):
     listings = ListingSerializer(many=True, read_only=True)
 
     class Meta:
         model = Auction
-        fields = ['id', 'auction_number', 'start_time', 'end_time', 'status', 'listings']
-        read_only_fields = ['id', 'listings']
+        fields = [
+            "id",
+            "auction_number",
+            "start_time",
+            "end_time",
+            "status",
+            "listings",
+        ]
+        read_only_fields = ["id", "listings"]

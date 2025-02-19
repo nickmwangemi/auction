d89bbd19ed443e2415efd9ee66b0995ec7104507
nickmwangemi <nickmwangemi@gmail.com>
2025-02-19 14:09:42 +0300

    linting

--- auction_api/views.py
+++ auction_api/views.py
@@ -5,15 +5,18 @@ from rest_framework.permissions import IsAuthenticated
 from auctions.models import Auction, Listing, Bid
 from .serializers import AuctionSerializer, ListingSerializer, BidSerializer
 
+
 class AuctionViewSet(viewsets.ModelViewSet):
     queryset = Auction.objects.all()
     serializer_class = AuctionSerializer
     permission_classes = [IsAuthenticated]
 
+
 class ListingViewSet(viewsets.ModelViewSet):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
     permission_classes = [IsAuthenticated]
+
 
 class BidViewSet(viewsets.ModelViewSet):
     queryset = Bid.objects.all()

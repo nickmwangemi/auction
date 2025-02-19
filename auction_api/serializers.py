1f893092706a611f042399a20fadd5633caa065e
nickmwangemi <nickmwangemi@gmail.com>
2025-02-19 14:09:02 +0300

    update api integration; viewsets and serializers

--- auction_api/serializers.py
+++ auction_api/serializers.py
@@ -1,21 +1,26 @@
 from rest_framework import serializers
 
-from auctions.models import Auction, Bid, Listing
-
-
-class AuctionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Auction
-        fields = "__all__"
-
-
-class ListingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Listing
-        fields = "__all__"
+from auctions.models import Bid, Listing, Auction
 
 
 class BidSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bid
-        fields = "__all__"
+        fields = ['id', 'listing', 'user', 'amount', 'timestamp', 'is_below_base_price']
+        read_only_fields = ['id', 'user', 'timestamp', 'is_below_base_price']
+
+class ListingSerializer(serializers.ModelSerializer):
+    bids = BidSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Listing
+        fields = ['id', 'auction', 'title', 'description', 'base_price', 'start_time', 'end_time', 'bids']
+        read_only_fields = ['id', 'bids']
+
+class AuctionSerializer(serializers.ModelSerializer):
+    listings = ListingSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Auction
+        fields = ['id', 'auction_number', 'start_time', 'end_time', 'status', 'listings']
+        read_only_fields = ['id', 'listings']
